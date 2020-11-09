beed0113e5bd4a40f7cad77418b665aeb797c377
Roman Vishnevskii <roman.vishnevsky@xored.com>
2020-11-10 02:42:53 +0700

    Add include and lib in build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 cd spreadsheet-engine
-g++-9 -O2 -std=c++17 -ltbb engine.cpp reader.cpp writer.cpp solutions/fast.cpp solutions/one-thread-simple.cpp -o ../engine.out
+g++-9 -O2 -std=c++17 -I/usr/local/Cellar/tbb/2020_U3_1/include/ -L/usr/local/Cellar/tbb/2020_U3_1/lib/ -ltbb engine.cpp reader.cpp writer.cpp solutions/fast.cpp solutions/one-thread-simple.cpp -o ../engine.out
