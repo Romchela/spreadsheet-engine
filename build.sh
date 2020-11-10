f647c8e0b9a75ff55eccab7abce65be24ed54a29
Roman Vishnevskii <roman.vishnevsky@xored.com>
2020-11-10 14:41:36 +0700

    dos2unix build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ cd spreadsheet-engine
 dos2unix inputs/*
 dos2unix outputs/*
 dos2unix ../run.sh
+
 g++-9 -O2 -std=c++17 -I/usr/local/Cellar/tbb/2020_U3_1/include/ -L/usr/local/Cellar/tbb/2020_U3_1/lib/ -ltbb engine.cpp reader.cpp writer.cpp solutions/fast.cpp solutions/one-thread-simple.cpp -o ../engine.out
